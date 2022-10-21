2eea1eae87b3f3cd9759fb1d1ea2ddbdc364ac15
mykhailo.murat <mykhailo.murat@gmail.com>
2022-10-21 17:48:09 +0300

    + front style + back styles

--- order-call.php
+++ order-call.php
@@ -21,6 +21,7 @@ class OrderCall
         add_shortcode('order_call_form', array($this, 'order_call_form'));
 
         add_action('admin_enqueue_scripts', [$this, 'enqueue_admin']);
+        add_action('wp_enqueue_scripts', [$this, 'enqueue_front']);
 
         add_action('admin_menu', [$this, 'add_admin_page']);
     }
@@ -53,38 +54,60 @@ class OrderCall
         wp_enqueue_script('ordercallScript', plugins_url('/admin/scripts.js', __FILE__));
     }
 
+    public function enqueue_front()
+    {
+        wp_enqueue_style('ordercallStyle', plugins_url('/styles.css', __FILE__));
+        wp_enqueue_script('ordercallScript', plugins_url('/scripts.js', __FILE__));
+    }
+
+
     static function order_call_form()
     {
         ob_start(); ?>
-        <form action="" method="post">
-            <div>
-                <label for="name">Name:</label>
-                <input type="text" name="name" required="required" placeholder="Enter your name"/>
-            </div>
-
-            <div>
-                <label for="name">Email:</label>
-                <input type="email" name="email" required="required" placeholder="Enter your email"/>
-            </div>
-
-            <div>
-                <label for="phone">Phone:</label>
-                <input type="tel" name="phone" required="required" placeholder="Enter your phone"/>
-            </div>
-
-            <button name="submit" type="submit">request a call</button>
-        </form>
+        <div class="order-call">
+            <form class="order-call__form" action="<? echo $_SERVER['PHP_SELF']?>" method="post">
+                <h3 class="order-call__form-title">We can call u</h3>
+                <input class="order-call__form-input" type="text" name="name" required="required"
+                       placeholder="Enter your name"/>
+                <input class="order-call__form-input" type="email" name="email" required="required"
+                       placeholder="Enter your email"/>
+                <input class="order-call__form-input" type="tel" name="phone" required="required"
+                       placeholder="Enter your phone"/>
+                <button class="order-call__form-submit" name="submit" type="submit">request a call</button>
+            </form>
+        </div>
         <?php
         $out = ob_get_clean();
 
         return $out;
     }
 
+    public static function form_handler($postArray)
+    {
+        if (isset($postArray['name'], $postArray['email'], $postArray['phone'])) {
+            global $wpdb;
+            $date = date('Y/m/d H:i:s');
+            $name = $_REQUEST['name'];
+            $email = $_REQUEST['email'];
+            $phone = $_REQUEST['phone'];
+            $table_name = $wpdb->prefix . 'ordercall_table';
+            $wpdb->insert($table_name, array(
+                'date' => $date,
+                'name' => $name,
+                'email' => $email,
+                'phone' => $phone
+            ));
+            echo "Thanks $name for your request.<br>";
+        } else {
+            echo 'You need to fill all fields';
+        }
+    }
+
     function add_admin_page()
     {
         // add top level menu page
         add_menu_page(
-            'all orders', //Page Title
+            'All Orders', //Page Title
             'Order Call', //Menu Title
             'manage_options', //Capability
             'order-call', //Page slug
@@ -106,17 +129,33 @@ class OrderCall
             // this will get the data from your table
             $retrieve_data = $wpdb->get_results("SELECT * FROM $table_name");
             ?>
-            <div class="db">
+            <div class="order-call">
+                <p class="order-call__info"><?php esc_html_e('you can use [order_call_form] shortcode to display form', 'default'); ?></p>
 
-                <?php foreach ($retrieve_data as $retrieved_data): ?>
-                    <ul class="db_row">
-                        <li><?php echo $retrieved_data->name; ?></li>
-                        <li><?php echo $retrieved_data->email; ?></li>
-                        <li><?php echo $retrieved_data->phone; ?></li>
-                        <li><?php echo $retrieved_data->date; ?></li>
-                    </ul>
-                <?php endforeach; ?>
-
+                <?php if ($retrieve_data): ?>
+                    <div class="order-call__table">
+                        <div class="order-call__table-labels order-call__table-row">
+                            <p><?php esc_html_e('Name', 'default'); ?></p>
+                            <p><?php esc_html_e('Email', 'default'); ?></p>
+                            <p><?php esc_html_e('Phone', 'default'); ?></p>
+                            <p><?php esc_html_e('Date', 'default'); ?></p>
+                        </div>
+                        <div class="order-call__table-body">
+                            <?php foreach ($retrieve_data as $index => $retrieved_data): ?>
+                                <div class="order-call__table-row <?php echo $index % 2 == 0 ? 'dark-row' : ''; ?>">
+                                    <div class="order-call__table-cell"><p><?php echo $retrieved_data->name; ?></p>
+                                    </div>
+                                    <div class="order-call__table-cell"><p><?php echo $retrieved_data->email; ?></p>
+                                    </div>
+                                    <div class="order-call__table-cell"><p><?php echo $retrieved_data->phone; ?></p>
+                                    </div>
+                                    <div class="order-call__table-cell"><p><?php echo $retrieved_data->date; ?></p>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
         <?php
@@ -125,25 +164,11 @@ class OrderCall
 
 if (class_exists('OrderCall')) {
     $orderCall = new OrderCall();
-//    $orderCall->form_handler();
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $formSub = new OrderCall();
+        $formSub->form_handler($_POST);
+        exit();
+    }
 };
 
 register_activation_hook(__FILE__, array($orderCall, 'insert_table_into_db'));
-
-if (isset($_REQUEST['name'], $_REQUEST['email'], $_REQUEST['phone'])) {
-    global $wpdb;
-    $date = date('Y/m/d H:i:s');
-    $name = $_REQUEST['name'];
-    $email = $_REQUEST['email'];
-    $phone = $_REQUEST['phone'];
-    $table_name = $wpdb->prefix . 'ordercall_table';
-    $wpdb->insert($table_name, array(
-        'date' => $date,
-        'name' => $name,
-        'email' => $email,
-        'phone' => $phone
-    ));
-    echo "Thanks $name for your request.<br>";
-} else {
-    echo 'You need to fill all fields';
-}
